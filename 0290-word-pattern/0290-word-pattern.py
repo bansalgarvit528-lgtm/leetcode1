5e3442c4b15fa592577f2db49a78ef44bb7f9720
bansalgarvit528-lgtm <bansalgarvit528@gmail.com>
2026-06-18 11:21:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.71%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        for i in range(len(pattern)):
+            if pattern.index(pattern[i]) != words.index(words[i]):
+                return False
+
+        return True
