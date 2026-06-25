3bc627ff26dc4b7e8cc43402b2d3b9f7adf6c577
bansalgarvit528-lgtm <bansalgarvit528@gmail.com>
2026-06-25 16:22:27 +0530

    Time: 2168 ms (37.21%) | Memory: 12.5 MB (97.67%) - LeetSync

---
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countMajoritySubarrays(self, nums, target):
+        ans = 0
+
+        for i in range(len(nums)):
+            count = 0
+
+            for j in range(i, len(nums)):
+                if nums[j] == target:
+                    count += 1
+
+                if count > (j - i + 1) // 2:
+                    ans += 1
+
+        return ans
