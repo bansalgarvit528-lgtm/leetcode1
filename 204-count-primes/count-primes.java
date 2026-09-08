b9388689e8e17a52666a5e11d7d2f10a159e5a89
Garvit Bansal <bansalgarvit528@gmail.com>
2026-09-08 12:38:47 +0530

    Time: 711 ms (20.77%) | Memory: 79.6 MB (28.15%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime,true);
+        isPrime[0] = false;
+        if(n>0){
+            isPrime[1] = false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]){
+                for(int j=i*i;j<=n;j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(isPrime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
